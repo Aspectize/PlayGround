eb366c69ca8a43288da61609cb082b42b1ae921b
Fredy <fredy@aspectize.com>
2017-11-29 15:43:58 +0100

    FF

--- PlayGroundRT/main.js
+++ PlayGroundRT/main.js
@@ -80,65 +80,36 @@ var playgroundSpecial = (function () {
             }
         };
     }
-    /*
-    function decodeUrl(url) {
-
-        var info = {};
-
-        var urlDecoder = /^(\w+):\/\/(?:(.+?)\/)(?:(\w+)\/)?(?:(\w+)\/)app\.ashx/;
-        var match = urlDecoder.exec(url);
-
-        if (match) {
-
-            info.HostPath = match[1] + "://" + match[2] + '/';
-            if (match[3]) info.HostPath += match[3] + "/";
-
-            info.ApplicationName = match[4];
-
-            info.ApplicationUrl = info.HostPath + info.ApplicationName + "/";
-
-            return info;
-
-        } else return null;
-    }
-*/
-    function init(initSvcName, versionKey) {
+  
+    function init(initSvcName) {
 
         var host = Aspectize.Host;
-        hst.ResourcesServiceName = initSvcName;
 
-        //var url = window.location.href;
+        host.ResourcesServiceName = initSvcName;
 
-        //var info = decodeUrl(url);
+        var versionKey = host.UrlArgs.Id;
 
-        //if (info) {
+        var url = window.location.href.split('?')[0];
+        var urlDecoder = /^(.*?)app\.ashx$/;
+        var match = urlDecoder.exec(url);
 
-        //var parts = url.split('?');
-        //if (parts.length === 2) {
-        if (versionKey) {
-            //var args = parts[1];
-            //var rxArg = /@Id\s*=\s*(\S+)/i;
-
-            //var m = rxArg.exec(args);
-
-            //if (m !== null) {
+        if (match && versionKey) {            
 
             window.AspectizeBuildDynamicViews = null;
 
             var cmdExt = ".js.cmd.ashx?versionKey=" + versionKey;
+            var svcPath = match[1] + initSvcName;
 
-            var cmdLib = info.ApplicationUrl + initSvcName + '.LoadJsLibrary' + cmdExt;
-            var cmdViews = info.ApplicationUrl + initSvcName + '.LoadJsViews' + cmdExt;
-            var cmdMain = info.ApplicationUrl + initSvcName + '.LoadMain' + cmdExt;
+            var cmdLib = svcPath + '.LoadJsLibrary' + cmdExt;
+            var cmdViews = svcPath + '.LoadJsViews' + cmdExt;
+            var cmdMain = svcPath + '.LoadMain' + cmdExt;
 
-            var scriptMan = newScriptManager();
+            scriptMan = newScriptManager();
 
             scriptMan.Load(cmdLib);
             scriptMan.Load(cmdViews);
             scriptMan.Load(cmdMain);
-        }
-        //}
-        //}
+        }       
     }
     function run(next) {
 
@@ -151,7 +122,7 @@ var playgroundSpecial = (function () {
 
 function OnApplicationStart() {
 
-    playgroundSpecial.Init('AppInitService', host.UrlArgs.Id);
+    playgroundSpecial.Init('AppInitService');
 }
 
 function Main() {
